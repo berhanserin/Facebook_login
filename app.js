c34faf81fe34e2fcb0525cfb1e40400c56a59882
Berhan <57452604+berhanserin@users.noreply.github.com>
2021-12-11 23:01:35 +0300

    facebook gizleme açılıd

--- app.js
+++ app.js
@@ -28,54 +28,54 @@ app.get('/auth/google',
 app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    // Successful authentication, redirect home.
+    
     res.redirect('/');
   });
 
-// passport.serializeUser(function (user, done) {
-//   done(null, user);
-// });
-// passport.deserializeUser(function (user, done) {
-//   done(null, user);
-// });
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: "440400450893972",
-//       clientSecret: "254f72bc5811cb7ae69fd2fe3927c96d",
-//       callbackURL: "http://localhost:5000/auth/facebook/callback",
-//       profileFields: ["id", "displayName", "photos", "email"],
-//     },
-//     function (accessToken, refreshToken, profile, done) {
-//       console.log(profile);
-//       const { email, first_name, last_name } = profile._raw;
-//       const userData = {
-//         email,
-//         firstName: first_name,
-//         lastName: last_name,
-//       };
-//       console.log(userData);
-//       done(null, profile);
-//     }
-//   )
-// );
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "440400450893972",
+      clientSecret: "254f72bc5811cb7ae69fd2fe3927c96d",
+      callbackURL: "http://localhost:5000/auth/facebook/callback",
+      profileFields: ["id", "displayName", "photos", "email"],
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log(profile);
+      const { email, first_name, last_name } = profile._raw;
+      const userData = {
+        email,
+        firstName: first_name,
+        lastName: last_name,
+      };
+      console.log(userData);
+      done(null, profile);
+    }
+  )
+);
 
-// app.get("/login", isLoggedIn, (req, res) => {
-//   res.send(`Hello world ${req.user.displayName}`);
-// });
+app.get("/login", isLoggedIn, (req, res) => {
+  res.send(`Hello world ${req.user.displayName}`);
+});
 
-// app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get("/auth/facebook", passport.authenticate("facebook"));
 
-// app.get(
-//   "/auth/facebook/callback",
-//   passport.authenticate("facebook", { failureRedirect: "/login" }),
-//   function (req, res) {
-//     console.log(req.user);
-//     req.logIn();
-//     res.redirect("/login");
-//   }
-// );
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function (req, res) {
+    console.log(req.user);
+    req.logIn();
+    res.redirect("/login");
+  }
+);
 
 app.listen(5000, () => {
   console.log(`Server Aktif 🚀🚀🚀🚀`);
